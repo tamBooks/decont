703ccff66eba0c0228ec666f397984a79dfe0d18
tamara <tamaraksaid@gmail.com>
2024-01-15 20:58:22 +0100

    Editing the Merging FastaQC code

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,3 +1,4 @@
+
 # This script should merge all files from a given sample (the sample id is
 # provided in the third argument ($3)) into a single file, which should be
 # stored in the output directory specified by the second argument ($2).
